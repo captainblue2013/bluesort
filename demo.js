f05ee6c523c65c814d3d3b28cb617178510fbf80
captainblue2013 <admin@lanhao.name>
2016-02-03 17:38:29 +0800

    insertion sort

---
+++ demo.js
@@ -0,0 +1,9 @@
+/**
+ * Created by lanhao on 16/2/3.
+ */
+
+var quickSort = require('./index').quickSort;
+
+var simpleSelection = require('./index').simpleSelection;
+
+console.log(quickSort([3,5,1,2,7,3,5,9,2,6,11,3,7],true));
--- index.js
+++ index.js
@@ -6,4 +6,8 @@ index.quickSort = require('./lib/quickSort');
 
 index.simpleSelection = require('./lib/simpleSelection');
 
+index.insertionSort = require('./lib/insertion');
+
 module.exports = index;
+
+//console.log(index.insertionSort([3,4,6,2,8,7,0,9,12],true));
